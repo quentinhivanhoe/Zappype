4b9c59e06ba65a4cf6adda3a33f6633baaefb54d
LoicR <loic.rabearivelo@epitech.eu>
2025-06-13 17:23:24 +0400

    feat(#25):bct commands

--- src/server/parsing/gui_commands.c
+++ src/server/parsing/gui_commands.c
@@ -44,3 +44,28 @@ void handle_mct(int client_fd, __attribute_maybe_unused__ char **cmd)
         }
     }
 }
+
+void handle_bct(int client_fd, char **cmd)
+{
+    size_t x = 0;
+    size_t y = 0;
+    tile_t tile = {0};
+
+    if (!cmd || !cmd[0] || !cmd[1] || !cmd[2]) {
+        dprintf(client_fd, "sbp\n");
+        return;
+    }
+    if (is_num(cmd[1]) == false || is_num(cmd[2]) == false) {
+        dprintf(client_fd, "sbp\n");
+        return;
+    }
+    x = atoi(cmd[1]);
+    y = atoi(cmd[2]);
+    tile = get_object(my_server()->map, x, y);
+    dprintf(client_fd,
+        "bct %zu %zu %ld %ld %ld %ld %ld %ld %ld\n", x, y,
+        tile.content[FOOD], tile.content[LINEMATE],
+        tile.content[DERAUMERE], tile.content[SIBUR],
+        tile.content[MENDIANE], tile.content[PHIRAS], tile.content[THYSTAME]
+    );
+}
