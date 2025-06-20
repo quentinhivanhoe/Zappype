99e02c0a40fa37a4a4101a4f064d5e836dc1d98d
SNicolas974 <nicolas.samy@epitech.eu>
2025-06-20 07:18:53 +0400

    feat: take command

--- src/server/parsing/ia_commands2.c
+++ src/server/parsing/ia_commands2.c
@@ -44,63 +44,53 @@ void handle_inventory(trn_t *trantorian, char **args)
     (void)args;
 }
 
-// static int get_object_index(trn_t *trantoran, const char *name)
-// {
-//     if (strcmp(name, "food") == 0) {
-//         trantoran->inventory[FOOD]++;
-//         return FOOD;
-//     }
-//     if (strcmp(name, "linemate") == 0) {
-//         trantoran->inventory[LINEMATE]++;
-//         return LINEMATE;
-//     }
-//     if (strcmp(name, "deraumere") == 0) {
-//         trantoran->inventory[DERAUMERE]++;
-//         return DERAUMERE;
-//     }
-//     if (strcmp(name, "sibur") == 0) {
-//         trantoran->inventory[SIBUR]++;
-//         return SIBUR;
-//     }
-//     if (strcmp(name, "mendiane") == 0) {
-//         trantoran->inventory[MENDIANE]++;
-//         return MENDIANE;
-//     }
-//     if (strcmp(name, "phiras") == 0) {
-//         trantoran->inventory[PHIRAS]++;
-//         return PHIRAS;
-//     }
-//     if (strcmp(name, "thystame") == 0) {
-//         trantoran->inventory[THYSTAME]++;
-//         return THYSTAME;
-//     }
-//     return -1;
-// }
+static int get_object_index(const char *name)
+{
+    if (strcmp(name, "food") == 0)
+        return FOOD;
+    if (strcmp(name, "linemate") == 0)
+        return LINEMATE;
+    if (strcmp(name, "deraumere") == 0)
+        return DERAUMERE;
+    if (strcmp(name, "sibur") == 0)
+        return SIBUR;
+    if (strcmp(name, "mendiane") == 0)
+        return MENDIANE;
+    if (strcmp(name, "phiras") == 0)
+        return PHIRAS;
+    if (strcmp(name, "thystame") == 0)
+        return THYSTAME;
+    return -1;
+}
 
-// void handle_take(trn_t *trantorian, char **args)
-// {
-//     int obj_idx = 0;
-//     uint64_t x = trantorian->pos.x;
-//     uint64_t y = trantorian->pos.y;
-//     tile_t *tile = &my_server()->map[x * my_server()->params.width + x];
+static void execute_take(trn_t *trantorian, int obj_idx)
+{
+    uint64_t x = trantorian->pos.x;
+    uint64_t y = trantorian->pos.y;
+    uint64_t width = my_server()->params.width;
+    tile_t *tile = &my_server()->map[y * width + x];
 
-//     if (!args || !args[0]) {
-//         dprintf(trantorian->socket, "ko1\n");
-//         return;
-//     }
-//     obj_idx = get_object_index(trantorian ,args[1]);
-//     printf("Pess lmk la i rode prend ça: %s\n", args[1]);
-//     if (obj_idx < 0) {
-//         dprintf(trantorian->socket, "ko2\n");
-//         return;
-//     }
-//     if (tile->content[obj_idx] == 0) {
-//         dprintf(trantorian->socket, "ko3\n");
-//         return;
-//     }
-//     dprintf(trantorian->socket, "ok\n");
-//     if (my_server()->params.debug_mode) {
-//         dprintf(2, "Take: %s, pos=(%lu,%lu), remaining=%lu\n",
-//                 args[0], x, y, tile->content[obj_idx]);
-//     }
-// }
+    if (tile->content[obj_idx] == 0) {
+        dprintf(trantorian->socket, "ko\n");
+        return;
+    }
+    tile->content[obj_idx]--;
+    trantorian->inventory[obj_idx]++;
+    dprintf(trantorian->socket, "ok\n");
+}
+
+void handle_take(trn_t *trantorian, char **args)
+{
+    int obj_idx;
+
+    if (!args || !args[1]) {
+        dprintf(trantorian->socket, "ko\n");
+        return;
+    }
+    obj_idx = get_object_index(args[1]);
+    if (obj_idx < 0) {
+        dprintf(trantorian->socket, "ko\n");
+        return;
+    }
+    execute_take(trantorian, obj_idx);
+}
